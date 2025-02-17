80e7b972fd2c98bbf6bb80126ee60e14841e54e4
NickOBlocksYT <Rodriguez.nick2009@gmail.com>
2025-02-17 16:02:09 -0500

    insanity

---
+++ src/main/java/com/nickoblocks/ftfmod/block/ModBlocks.java
@@ -0,0 +1,46 @@
+package com.nickoblocks.ftfmod.block;
+
+import com.nickoblocks.ftfmod.FTFMod;
+import com.nickoblocks.ftfmod.item.ModItems;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.SoundType;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+import java.util.function.Supplier;
+
+public class ModBlocks {
+
+    public static final DeferredRegister<Block> BLOCKS =
+            DeferredRegister.create(ForgeRegistries.BLOCKS, FTFMod.MOD_ID);
+
+
+
+    public static final RegistryObject<Block> NICKCOIN_PILE = registerBlock("nickcoin_pile",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(5f).sound(SoundType.COPPER)));
+    public static final RegistryObject<Block> COBBLED_NICITE = registerBlock("cobbled_nicite",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(3f).sound(SoundType.STONE)));
+
+
+
+    private static <T extends Block> RegistryObject<T> registerBlock(String name, Supplier<T> block) {
+        RegistryObject<T> toReturn = BLOCKS.register(name, block);
+        registerBlockItem(name, toReturn);
+        return toReturn;
+    }
+
+    private static <T extends Block> void registerBlockItem(String name, RegistryObject<T> block){
+        ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
+    }
+
+    public static void register(IEventBus eventBus){
+        BLOCKS.register(eventBus);
+    }
+}
---
+++ src/main/java/com/nickoblocks/ftfmod/item/ModItems.java
@@ -0,0 +1,22 @@
+package com.nickoblocks.ftfmod.item;
+
+import com.nickoblocks.ftfmod.FTFMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, FTFMod.MOD_ID);
+
+    public static final RegistryObject<Item> NICKCOIN = ITEMS.register("nickcoin",
+            () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> NICITE_CLUMP = ITEMS.register("nicite_clump",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
